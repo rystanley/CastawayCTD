e9cfa033c576af463cfcb5562e12b6115a34a002
Homarus23 <rstanley@mun.ca>
2017-11-07 22:19:05 -0400

    didn't actually make it a function ... duh

--- CTDProcessBatch.R
+++ CTDProcessBatch.R
@@ -1,4 +1,4 @@
-CTDProcess_Batch(paths,datetime=T){
+CTDProcess_Batch <- function(paths,datetime=T){
 
   #paths are a vector of file paths which point to the Castaway csv output files
   #datetime - parameter (default is true) which will split the date and time information into individual columns
