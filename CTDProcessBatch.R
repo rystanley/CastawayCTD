9ddd9568c2b3f9ddaea9b8db69bfc94ed2f8e1a3
Homarus23 <rstanley@mun.ca>
2017-11-07 22:24:48 -0400

    Overhaul

--- CTDProcessBatch.R
+++
@@ -1,8 +0,0 @@
-CTDProcess_Batch <- function(paths,datetime=T){
-
-  #paths are a vector of file paths which point to the Castaway csv output files
-  #datetime - parameter (default is true) which will split the date and time information into individual columns
-  
-return(do.call("rbind", lapply(filepaths, function(x) CTDProcess(x,datetime=datetime))))
-
-}
